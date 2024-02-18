e6ad165341ea7c12b577da049c3150c9ee3f4824
Mafit1 <filippov.mops@yandex.ru>
2024-02-18 14:17:52 +0300

    123

---
+++ app/src/main/java/com/example/filippov_mihail/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.filippov_mihail;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity{
+
+    public final static String EXTRA_MESSAGE = "com.example.filippov_mihail.MESSAGE";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
